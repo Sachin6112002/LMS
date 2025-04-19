774e4d61be88360a76136ae81caa2b602a4d6168
Sachin <sachinkaintura2002@gmail.com>
2025-04-19 22:04:02 +0530

    added the getEducatorCourses API

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
-// import { educatorDahboardData, getEducatorCourses, getEnrolledStudentsData} from  '../controllers/educatorController.js'
-import {updateRoleToEducator , addCourse} from  '../controllers/educatorController.js'
+// import { educatorDahboardData, getEnrolledStudentsData} from  '../controllers/educatorController.js'
+import {updateRoleToEducator , addCourse , getEducatorCourses} from  '../controllers/educatorController.js'
 
 import protectEducator from '../middlewares/authMiddleware.js'
 import upload from '../configs/multer.js'
@@ -8,7 +8,7 @@ const educatorRouter = express.Router()
 //Add Educator Role
 educatorRouter.get('/update-role', updateRoleToEducator)
 educatorRouter.post('/add-course', upload.single('image'), protectEducator,addCourse)
-// educatorRouter.get('/courses', protectEducator, getEducatorCourses)
+ educatorRouter.get('/courses', protectEducator, getEducatorCourses)
 // educatorRouter.get('/dashboard', protectEducator, educatorDahboardData )
 // educatorRouter.get('/enrolled-students', protectEducator, getEnrolledStudentsData)
 export default educatorRouter;
