d1fde7295c7202c8485d16dc3f9832cf220c4490
Sachin <sachinkaintura2002@gmail.com>
2025-04-19 21:05:31 +0530

    added add-course function

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
-// import {addCourse, educatorDahboardData, getEducatorCourses, getEnrolledStudentsData, updateRoleToEducator} from  '../controllers/educatorController.js'
-import {updateRoleToEducator} from  '../controllers/educatorController.js'
+// import { educatorDahboardData, getEducatorCourses, getEnrolledStudentsData} from  '../controllers/educatorController.js'
+import {updateRoleToEducator , addCourse} from  '../controllers/educatorController.js'
 
-//import protectEducator from '../middlewares/authMiddleware.js'
-//import upload from '../configs/multer.js'
+import protectEducator from '../middlewares/authMiddleware.js'
+import upload from '../configs/multer.js'
 const educatorRouter = express.Router()
 //Add Educator Role
 educatorRouter.get('/update-role', updateRoleToEducator)
-// educatorRouter.post('/add-course', upload.single('image'), protectEducator,addCourse)
+educatorRouter.post('/add-course', upload.single('image'), protectEducator,addCourse)
 // educatorRouter.get('/courses', protectEducator, getEducatorCourses)
 // educatorRouter.get('/dashboard', protectEducator, educatorDahboardData )
 // educatorRouter.get('/enrolled-students', protectEducator, getEnrolledStudentsData)
