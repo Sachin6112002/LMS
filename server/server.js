87fc2c8520532b2449987f4aca3e204dcdbd09a4
Sachin <sachinkaintura2002@gmail.com>
2025-04-24 11:59:55 +0530

    just resolved some silly mistakes

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.post('/clerk', express.raw({ type: 'application/json' }), clerkWebhooks)
 app.use('/api/educator' , express.json(), educatorRouter)
  app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user', express.json(), userRouter)
-app.post('/stripe' , express.raw({type: 'applcation/json'}), stripeWebhooks)
+app.post('/stripe' , express.raw({type: 'application/json'}), stripeWebhooks)
 
 //PORT
 const PORT = process.env.PORT || 3000
