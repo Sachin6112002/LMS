cf5657c9b9d7b87d7e0ead33a3c0e206d7f22ae4
Sachin <sachinkaintura2002@gmail.com>
2025-05-02 10:10:43 +0530

    fixing some issues related to the user add-course

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -5,52 +5,49 @@ import Course from "../models/Course.js";
 import { Purchase } from "../models/Purchase.js";
 
 export const clerkWebhooks = async (req, res) => {
-  try {
-    const payload = req.body; // raw buffer
-    const headers = {
-      "svix-id": req.headers["svix-id"],
-      "svix-timestamp": req.headers["svix-timestamp"],
-      "svix-signature": req.headers["svix-signature"],
-    };
-
-    const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    const evt = wh.verify(payload, headers);
-
-    const { data, type } = evt;
-
-    switch (type) {
-      case "user.created": {
-        const userData = {
-          _id: data.id,
-          email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
-          imageUrl: data.image_url,
-        };
-        await User.create(userData);
-        break;
+    try {
+      const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+      await whook.verify(JSON.stringify(req.body), {
+        "svix-id": req.headers["svix-id"],
+        "svix-timestamp": req.headers["svix-timestamp"],
+        "svix-signature": req.headers["svix-signature"],
+      });
+      const { data, type } = req.body;
+      switch (type) {
+        case "user.created": {
+          const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            name: data.first_name + " " + data.last_name,
+            imageUrl: data.image_url,
+          };
+          await User.create(userData);
+          res.json({});
+          break;
+        }
+        case "user.updated": {
+          const userData = {
+            email: data.email_addresses[0].email_address,
+            name: data.first_name + " " + data.last_name,
+            imageUrl: data.image_url,
+          };
+          await User.findByIdAndUpdate(data.id, userData);
+          res.json({});
+  
+          break;
+        }
+        case "user.selected": {
+          await User.findByIdAndDelete(data.id);
+          res.json({});
+          break;
+        }
+        default:
+          break;
       }
-      case "user.updated": {
-        const userData = {
-          email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
-          imageUrl: data.image_url,
-        };
-        await User.findByIdAndUpdate(data.id, userData);
-        break;
-      }
-      case "user.deleted": {
-        await User.findByIdAndDelete(data.id);
-        break;
-      }
-      default:
-        console.log("Unhandled Clerk event type:", type);
+    } catch (error) {
+      res.json({ success: false, message: error.message });
     }
-
-    res.status(200).json({ success: true });
-  } catch (error) {
-    res.status(400).json({ success: false, message: error.message });
-  }
-};
+  };
 
 const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
 export const stripeWebhooks = async (request, response) => {
