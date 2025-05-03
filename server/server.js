8b6258d12ccd0022e6170e4a237edb11c7b1c4bd
Sachin <sachinkaintura2002@gmail.com>
2025-05-03 11:59:47 +0530

    gatewaytimeout problem resolve

--- server/server.js
+++ server/server.js
@@ -1,35 +1,35 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import { clerkWebhooks, stripeWebhooks } from "./controllers/webhooks.js";
+import  express from "express"
+import cors from 'cors'
+import 'dotenv/config'
 import connectDB from "./configs/mongodb.js";
-import connectCloudinary from "./configs/cloudinary.js";
-import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhooks, stripeWebhooks } from "./controllers/webhooks.js";
 import educatorRouter from "./routes/educatorRoutes.js";
-import courseRouter from "./routes/courseRoute.js";
-import userRouter from "./routes/userRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
+import connectCloudinary from "./configs/cloudinary.js";
+ import courseRouter from "./routes/courseRoute.js";
+ import userRouter from "./routes/userRoutes.js";
+//Initialize Express
 
-const app = express();
+const app = express()
+//Connect to database
+await connectDB()
+await connectCloudinary()
+//Middlewares
 app.use(cors())
-// Connect DB and Cloudinary
-await connectDB();
-await connectCloudinary();
-app.post("/clerk"  , express.json() , clerkWebhooks)
-app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
+app.use(clerkMiddleware())
 
-// Clerk middleware should be applied AFTER webhook routes
-app.use(clerkMiddleware());
-app.use(express.json()); // ⬅️ for routes that need JSON parsing
+//Routes
+app.get('/', (req, res) => res.send("API is working"))
+app.post('/clerk', express.raw({ type: 'application/json' }), clerkWebhooks)
 
-// Routes
-app.get("/", (req, res) => res.send("API is working"));
-app.use("/api/educator", educatorRouter);
-app.use("/api/course", courseRouter);
-app.use("/api/user", userRouter);
+app.use('/api/educator' , express.json(), educatorRouter)
+ app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe' , express.raw({type: 'applcation/json'}), stripeWebhooks)
 
-// Start Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Server started on port ${PORT}`);
-  console.log(`🧪 Clerk webhook ready at POST /clerk`);
-});
+//PORT
+const PORT = process.env.PORT || 3000
+app.listen(PORT , ()=>{
+    console.log("welcome to my new server ");
+    
+})
