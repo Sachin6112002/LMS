1879d9ebf95350671c9b5af02fc44b82c7095f01
Sachin <sachinkaintura2002@gmail.com>
2025-05-05 13:12:08 +0530

    fixed some small bug in the userRouter

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import {addUserRating, getUserCourseProgess, getUserData , purchaseCourse, updateUserCourseProgress, userErolledCourses} from "../controllers/userController.js"
+import {addUserRating, getUserCourseProgress, getUserData , purchaseCourse, updateUserCourseProgress, userErolledCourses} from "../controllers/userController.js"
 const userRouter = express.Router()
 userRouter.get('/data', getUserData)
 userRouter.get('/enrolled-courses',userErolledCourses)
 userRouter.post('/purchase', purchaseCourse)
 userRouter.post('/update-course-progress', updateUserCourseProgress)
-userRouter.post('/get-course-progress', getUserCourseProgess)
+userRouter.post('/get-course-progress', getUserCourseProgress)
 userRouter.post('/add-rating' , addUserRating)
 
 export default userRouter;  
