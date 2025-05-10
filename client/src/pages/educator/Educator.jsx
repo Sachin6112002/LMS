d44824098a010bda8a6d43c9905e65aa3012bf25
Sachin <sachinkaintura2002@gmail.com>
2025-05-10 15:24:58 +0530

    added updated context

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import SideBar from '../../components/educator/SideBar'
+import SideBar from '../components/educator/SideBar'
 import Navbar from '../../components/educator/Navbar'
 import Footer from '../../components/educator/Footer'
 
