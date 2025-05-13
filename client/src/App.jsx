8529df95c4de711e1266787d7ce7936f43599f6c
Sachin <sachinkaintura2002@gmail.com>
2025-05-13 21:57:10 +0530

    resolving problem related to the admin pannel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,25 @@
-import React from 'react'
-import { Route, Routes, useMatch } from 'react-router-dom'
-import Home from './pages/student/Home'
-import CoursesList from './pages/student/CoursesList'
-import CourseDetails from './pages/student/CourseDetails'
-import MyEnrollments from './pages/student/MyEnrollments'
-import Player from './pages/student/Player'
-import Loading from './components/student/Loading'
-import Educator from './pages/educator/Educator'
-import Dashboard from './pages/educator/Dashboard'
-import AddCourse from './pages/educator/AddCourse'
-import MyCourses from './pages/educator/MyCourses'
-import StudentsEnrolled from './pages/educator/StudentsEnrolled'
-import Navbar from './components/student/Navbar'
-import Admin from './pages/admin/Admin';
-import AdminDashboard from './pages/admin/AdminDahboard';
-import AdminSettings from './pages/admin/AdminSettings';
-import ManageCourses from './pages/admin/ManageCourses';
-import ManageUsers from './pages/admin/ManageUsers';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+ // Admin-specific Navbar
+import Footer from '../../components/admin/Footer'; // Admin-specific Footer
+import AdminSidebar from '../../components/admin/SideBar';
+import AdminNavbar from '../../components/admin/Navbar'; // Admin-specific Navbar
+import AdminProfile from '../../components/admin/Profile'; // Admin-specific Profile Component
 
-const App = () => {
-  const isEducatorRoute = useMatch('/educator/*')
-  const isAdminRoute = useMatch('/admin/*')
+const Admin = () => {
   return (
-    <div className='text-default min-h-screen bg-white'>
-      {!isAdminRoute && !isEducatorRoute && <Navbar/>}
-      
-    <Routes>
-    <Route path = '/' element = {<Home/>}/>
-    <Route path = '/course-list' element = {<CoursesList/>}/>
-    <Route path = '/course-list/:input' element = {<CoursesList/>}/>
-    <Route path = '/course/:id' element = {<CourseDetails />}/>
-    <Route path = '/my-enrollments' element = {<MyEnrollments />}/>
-    <Route path = '/player/:courseId' element = {<Player />}/>
-    <Route path = '/loading/:path' element = {<Loading/>}/>
-      <Route path = '/educator' element= {<Educator/>}>
-      <Route path='/educator' element={<Dashboard/>}/>
-      <Route path='add-course' element={<AddCourse/>}/>
-      <Route path='my-courses' element={<MyCourses/>}/>
-      <Route path='student-enrolled' element={<StudentsEnrolled/>}/>
-      </Route>
-      <Route path='/admin' element={<Admin/>}>
-      <Route path='dashboard' element={<AdminDashboard/>}/>
-      <Route path='settings' element={<AdminSettings/>}/>
-      <Route path='manage-courses' element={<ManageCourses/>}/>
-      <Route path='manage-users' element={<ManageUsers/>}/>
-      </Route>
-    </Routes>
-    
-
+    <div className="text-default min-h-screen bg-white"> {/* Changed background color to white for better visibility */}
+      <AdminNavbar /> {/* Updated to use Admin-specific Navbar */}
+      <div className="flex">
+        <AdminSidebar />
+        <div className="flex-1">
+          <Outlet />
+          <AdminProfile /> {/* Added Admin Profile to the right-hand side */}
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default Admin;
--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
- // Admin-specific Navbar
-import Footer from '../../components/admin/Footer'; // Admin-specific Footer
+import Footer from '../../components/admin/Footer';
 import AdminSidebar from '../../components/admin/SideBar';
-import AdminNavbar from '../../components/admin/Navbar'; // Admin-specific Navbar
-import AdminProfile from '../../components/admin/Profile'; // Admin-specific Profile Component
+import AdminNavbar from '../../components/admin/Navbar';
+import AdminProfile from '../../components/admin/Profile';
 
 const Admin = () => {
   return (
-    <div className="text-default min-h-screen bg-white"> {/* Changed background color to white for better visibility */}
-      <AdminNavbar /> {/* Updated to use Admin-specific Navbar */}
+    <div className="text-default min-h-screen bg-white">
+      <AdminNavbar />
       <div className="flex">
         <AdminSidebar />
         <div className="flex-1">
-          <Outlet />
-          <AdminProfile /> {/* Added Admin Profile to the right-hand side */}
+          <Outlet /> {/* Ensures child routes render here */}
+          <AdminProfile />
         </div>
       </div>
       <Footer />
