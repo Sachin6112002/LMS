47df9b57700c22c72189f791830dd8a565ed441a
Sachin <sachinkaintura2002@gmail.com>
2025-05-13 21:33:28 +0530

    admin profile

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../../components/admin/Navbar'; // Admin-specific Navbar
+ // Admin-specific Navbar
 import Footer from '../../components/admin/Footer'; // Admin-specific Footer
 import AdminSidebar from '../../components/admin/SideBar';
+import AdminNavbar from '../../components/admin/Navbar'; // Admin-specific Navbar
+import AdminProfile from '../../components/admin/Profile'; // Admin-specific Profile Component
 
 const Admin = () => {
   return (
-    <div className="text-default min-h-screen bg-red">
- <Navbar/>
+    <div className="text-default min-h-screen bg-white"> {/* Changed background color to white for better visibility */}
+      <AdminNavbar /> {/* Updated to use Admin-specific Navbar */}
       <div className="flex">
         <AdminSidebar />
         <div className="flex-1">
           <Outlet />
+          <AdminProfile /> {/* Added Admin Profile to the right-hand side */}
         </div>
       </div>
       <Footer />
