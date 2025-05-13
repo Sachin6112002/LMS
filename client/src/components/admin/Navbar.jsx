45a37ec8c80ea7ffbcdb5d5b173e5bf13fe3ea9f
Sachin <sachinkaintura2002@gmail.com>
2025-05-13 21:27:47 +0530

    added new component

--- client/src/components/admin/Navbar.jsx
+++ client/src/components/admin/Navbar.jsx
@@ -5,23 +5,19 @@ import { AppContext } from '../../context/AppContext';
 import { UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = ({ bgColor }) => {
+  const { user } = useUser();
 
-  const { isEducator } = useContext(AppContext)
-  const { user } = useUser()
-
-  return isEducator && user && (
+  return (
     <div className={`flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3 ${bgColor}`}>
       <Link to="/">
         <img src={assets.logo} alt="Logo" className="w-28 lg:w-32" />
       </Link>
       <div className="flex items-center gap-5 text-gray-500 relative">
-        <p>Hi! {user.fullName}</p>
+        {user && <p>Hi! {user.fullName}</p>}
         <UserButton />
-        {user.isAdmin && (
-          <Link to="/admin" className="text-blue-500 hover:underline">
-            Admin Panel
-          </Link>
-        )}
+        <Link to="/admin" className="text-blue-500 hover:underline">
+          Admin Panel
+        </Link>
       </div>
     </div>
   );
