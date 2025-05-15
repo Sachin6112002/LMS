2495a92b21bc0ccc1ec0a353c27988e41cf2ce9f
Sachin <sachinkaintura2002@gmail.com>
2025-05-15 11:50:01 +0530

    addded updated code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,26 @@
-import React, { useContext } from 'react'
-import { Routes, Route, useLocation, useMatch } from 'react-router-dom'
-import Navbar from './components/student/Navbar'
-import Home from './pages/student/Home'
-import CourseDetails from './pages/student/CourseDetails'
-import CoursesList from './pages/student/CoursesList'
-import Dashboard from './pages/educator/Dashboard'
-import AddCourse from './pages/educator/AddCourse'
-import MyCourses from './pages/educator/MyCourses'
-import StudentsEnrolled from './pages/educator/StudentsEnrolled'
-import Educator from './pages/educator/Educator'
-import 'quill/dist/quill.snow.css'
+import React, { useContext } from 'react';
+import { Routes, Route, useMatch } from 'react-router-dom';
+import Navbar from './components/student/Navbar';
+import Home from './pages/student/Home';
+import CourseDetails from './pages/student/CourseDetails';
+import CoursesList from './pages/student/CoursesList';
+import Dashboard from './pages/educator/Dashboard';
+import AddCourse from './pages/educator/AddCourse';
+import MyCourses from './pages/educator/MyCourses';
+import StudentsEnrolled from './pages/educator/StudentsEnrolled';
+import Educator from './pages/educator/Educator';
+import 'quill/dist/quill.snow.css';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify'
-import Player from './pages/student/Player'
-import MyEnrollments from './pages/student/MyEnrollments'
-import Loading from './components/student/Loading'
+import { ToastContainer } from 'react-toastify';
+import Player from './pages/student/Player';
+import MyEnrollments from './pages/student/MyEnrollments';
+import Loading from './components/student/Loading';
+import Admin from './pages/admin/Admin';
+import ManageUsers from './pages/admin/ManageUsers';
+import AdminSettings from './pages/admin/AdminSettings';
+import AdminDashboard from './pages/admin/AdminDahboard';
 
 const App = () => {
-
   const isEducatorRoute = useMatch('/educator/*');
 
   return (
@@ -26,6 +29,7 @@ const App = () => {
       {/* Render Student Navbar only if not on educator routes */}
       {!isEducatorRoute && <Navbar />}
       <Routes>
+        {/* Student Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/course/:id" element={<CourseDetails />} />
         <Route path="/course-list" element={<CoursesList />} />
@@ -33,20 +37,24 @@ const App = () => {
         <Route path="/my-enrollments" element={<MyEnrollments />} />
         <Route path="/player/:courseId" element={<Player />} />
         <Route path="/loading/:path" element={<Loading />} />
-        <Route path='/educator' element={<Educator />}>
-          <Route path='/educator' element={<Dashboard />} />
-          <Route path='add-course' element={<AddCourse />} />
-          <Route path='my-courses' element={<MyCourses />} />
-          <Route path='student-enrolled' element={<StudentsEnrolled />} />
-       
-        <Route path='/admin/*' element={<Admin />}></Route>
-          <Route path='/admin/dashboard' element={<AdminDashboard />} />
-          <Route path='/admin/manage-users' element={<ManageUsers />} />
-          <Route path='/admin/settings' element={<AdminSettings />} />
+
+        {/* Educator Routes */}
+        <Route path="/educator" element={<Educator />}>
+          <Route path="/educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="student-enrolled" element={<StudentsEnrolled />} />
+        </Route>
+
+        {/* Admin Routes */}
+        <Route path="/admin" element={<Admin />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="manage-users" element={<ManageUsers />} />
+          <Route path="settings" element={<AdminSettings />} />
         </Route>
       </Routes>
     </div>
   );
 };
 
-export default Admin;
+export default App;
