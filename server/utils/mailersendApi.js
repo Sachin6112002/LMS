51c5080754d265bee3e17088877ad8d06a8c3a78
Sachin <sachinkaintura2002@gmail.com>
2025-06-21 16:54:44 +0530

    email related issues

--- server/utils/mailersendApi.js
+++ server/utils/mailersendApi.js
@@ -6,11 +6,19 @@ const mailersend = new MailerSend({
 });
 
 export async function sendMail({ to, subject, text, html }) {
+  console.log('[MailerSend] sendMail called with:', { to, subject });
   const emailParams = new EmailParams()
     .setFrom(new Sender(process.env.MAILERSEND_FROM, 'eSiksha'))
     .setTo([new Recipient(to, to)])
     .setSubject(subject)
     .setText(text)
     .setHtml(html);
-  return mailersend.email.send(emailParams);
+  try {
+    const response = await mailersend.email.send(emailParams);
+    console.log('[MailerSend] Email sent successfully:', response);
+    return response;
+  } catch (error) {
+    console.error('[MailerSend] Error sending email:', error);
+    throw error;
+  }
 }
