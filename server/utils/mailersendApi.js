bb5567ebc1da8f070998ed5be297ebaa9070754c
Sachin <sachinkaintura2002@gmail.com>
2025-06-23 10:21:09 +0530

    some problem in mailer sender

--- server/utils/mailersendApi.js
+++ server/utils/mailersendApi.js
@@ -1,23 +1,2 @@
-// mailersendApi.js (This file is now obsolete and replaced by resendApi.js using Resend)
-// You can delete this file.
-
-// import { MailerSend } from 'mailersend';
-//
-// const mailerSend = new MailerSend({ apiKey: process.env.MAILERSEND_API_KEY });
-//
-// export async function sendMail({ to, subject, text, html }) {
-//   try {
-//     const response = await mailerSend.send({
-//       from: process.env.MAILERSEND_FROM, // e.g. 'YourApp <noreply@yourdomain.com>'
-//       to,
-//       subject,
-//       text,
-//       html,
-//     });
-//     console.log('[MailerSend] Email sent successfully:', response);
-//     return response;
-//   } catch (error) {
-//     console.error('[MailerSend] Error sending email:', error);
-//     throw error;
-//   }
-// }
+// This file is obsolete and replaced by resendApi.js using Resend.
+// You can delete this file from your project.
