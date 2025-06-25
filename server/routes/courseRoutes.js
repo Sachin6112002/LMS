5af80ed8f9b13f0afb61e340c96ea2a87caeda83
Sachin <sachinkaintura2002@gmail.com>
2025-06-25 22:30:00 +0530

    401 error

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { jwtMiddleware } from '../middlewares/jwtMiddleware.js';
 import upload, { videoUpload } from '../configs/multer.js';
 import {
   createCourse,
@@ -11,13 +12,13 @@ import {
 const router = express.Router();
 
 // Create a course with metadata and thumbnail
-router.post('/', authMiddleware, upload.single('thumbnail'), createCourse);
+router.post('/', jwtMiddleware, authMiddleware, upload.single('thumbnail'), createCourse);
 
 // Add chapter to a course
-router.post('/:id/chapters', authMiddleware, addChapter);
+router.post('/:id/chapters', jwtMiddleware, authMiddleware, addChapter);
 
 // Upload video and add lecture to a chapter
-router.post('/:id/chapters/:chapterIndex/lectures', authMiddleware, videoUpload.single('video'), addLecture);
+router.post('/:id/chapters/:chapterIndex/lectures', jwtMiddleware, authMiddleware, videoUpload.single('video'), addLecture);
 
 // Get all courses
 router.get('/', getAllCourses);
