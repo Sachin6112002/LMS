013c2f32083d60dc7ea24e638b08de968aa89237
Sachin <sachinkaintura2002@gmail.com>
2025-06-29 12:17:49 +0530

    some issue erlaed to mime

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Updated to port 3000
+        target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
     },
   },
+  base: '/',
 })
