103236382b5636e050aea19130b3d02dfe0eea65
Sachin <sachinkaintura2002@gmail.com>
2025-07-01 09:04:41 +0530

    wrong name

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -96,7 +96,7 @@ const MyEnrollments = () => {
                         <div key={course._id} className="bg-white border border-green-200 rounded-lg shadow p-6 flex flex-col gap-4">
                             <h2 className="text-xl font-semibold text-green-900">{course.title}</h2>
                             <p className="text-green-700">{
-                                typeof course.createdBy === 'object' && course.createdBy?.name
+                                typeof course.createdBy === 'object' && course.createdBy?.name && course.createdBy.name.length < 30 && isNaN(Number(course.createdBy.name))
                                     ? course.createdBy.name
                                     : (typeof course.createdBy === 'string' && course.createdBy.length < 30 && isNaN(Number(course.createdBy))
                                         ? course.createdBy
