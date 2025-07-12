cc0cd0b0d175561b3194c0c86e25229720498dfc
Sachin <sachinkaintura2002@gmail.com>
2025-07-12 09:42:42 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -50,7 +50,9 @@ app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
       return res.status(200).end();
     }
-  } else if (allowedOrigins.includes(origin)) {
+    return next();
+  }
+  if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
