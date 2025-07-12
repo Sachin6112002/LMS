7bb67c204a211e45c5c05ccefb45aea91cd2b532
Sachin <sachinkaintura2002@gmail.com>
2025-07-12 22:44:14 +0530

    course price  is 0

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -79,8 +79,8 @@ const AddCourse = () => {
       const formData = new FormData();
       formData.append('title', courseTitle);
       formData.append('description', quillRef.current.root.innerHTML);
-      formData.append('price', coursePrice);
-      formData.append('discount', discount);
+      formData.append('price', Number(coursePrice));
+      formData.append('discount', Number(discount));
       formData.append('image', image);
       const token = await getToken();
       const { data } = await axios.post(
